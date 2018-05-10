5853cbaac5f0be369109d4e246f83f9440e79eca
李立冬 <lilidong@dragonest.com>
2018-05-10 18:43:55 +0800

    修复内存泄露

--- db.js
+++ db.js
@@ -11,6 +11,52 @@
  });
 
 
+//way1
+// If the Node process ends, close the Mongoose connection （效果不怎么好可以删除 看mysql版本的api）
+// process.on( 'SIGINT', function() {  
+//     conn.end( function ( err ) {
+//         console.log( 'MySQL connection disconnected through app termination' );
+//         process.exit( 0 ) ; 
+//     } ); 
+// } );
+// 
+
+//报错(内存泄露 导致了这个错误 用way2 的方式 就ok)
+//spider3 > (node:30211) [DEP0096] DeprecationWarning: timers.unenroll() is deprecated. Please use clearTimeout instead.  
+
+
+//推荐使用这个 pm2 内存占用低 cpu使用也较低 这个是内存泄漏最好的补救
+//way2
+// If the Node process ends, close the Mongoose connection （没有效果可以删除 看mysql版本的api）
+process.on('SIGINT', () => { 
+      const cleanUp = () => {
+         conn.end(console.error);
+         process.exit( 0 ) ; 
+      };
+
+      server.close(() => {
+        // Stop after 10 secs
+        setTimeout(() => {
+          cleanUp();
+          process.exit();
+        }, 10000);
+      });
+
+      // Force close server after 15 secs
+      setTimeout((e) => {
+        console.log('Forcing server close !!!', e);
+        cleanUp();
+        process.exit(1);
+      }, 15000);
+
+});
+
+process.on( 'SIGINT', function(){
+    console.log( 'gracefully shutting down :)' );
+    conn.end();
+    process.exit();
+});
+
 
 module.exports = new Promise((resolve, reject) => {
     conn.connect((err) => {
